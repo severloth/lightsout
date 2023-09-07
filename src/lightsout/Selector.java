3efe2c0ea59d1dc8f0e369975cf6fa4bf4146999
micaoca <miccaoca@gmail.com>
2023-09-06 22:23:17 -0300

    modificaciones en  selector

--- src/lightsout/Selector.java
+++ src/lightsout/Selector.java
@@ -2,15 +2,16 @@ package lightsout;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
-import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JCheckBox;
+import java.awt.Font;
+import java.awt.SystemColor;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Selector {
 	private JFrame frame;
@@ -21,7 +22,39 @@ public class Selector {
 	public Selector() {
 		this.tamanioSeleccionado = 0;
 		this.frame = new JFrame();
-		this.panel = new JPanel(new GridLayout(1, 3));
+		this.panel = new JPanel();
+		panel.setBackground(SystemColor.menu);
+		panel.setForeground(SystemColor.windowBorder);
+		panel.setLayout(null);
+		
+		this.botonConfirmarSeleccion = new JButton("Comenzar juego");
+		botonConfirmarSeleccion.setBackground(SystemColor.controlHighlight);
+		botonConfirmarSeleccion.setFont(new Font("Yu Gothic UI", Font.BOLD, 12));
+		botonConfirmarSeleccion.setBounds(188, 242, 160, 22);
+		panel.add(botonConfirmarSeleccion);
+		
+		JLabel lblNewLabel = new JLabel("LightsOut");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Yu Gothic UI", Font.BOLD, 30));
+		lblNewLabel.setBounds(190, 0, 155, 72);
+		panel.add(lblNewLabel);
+		
+		JButton botoncuatro = crearBoton("4 x 4", 4);
+		botoncuatro.setBounds(80, 120, 125, 80);
+		panel.add(botoncuatro);
+		
+		JButton botoncinco = crearBoton("5 x 5", 5);
+		botoncinco.setBounds(210, 120, 125, 80);
+		panel.add(botoncinco);
+		
+		JButton botonseis = crearBoton("7 x 7", 7);
+		botonseis.setBounds(340, 120, 125, 80);
+		panel.add(botonseis);
+		
+		JLabel lblNewLabel_1 = new JLabel("Elegir tama\u00F1o del tablero");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(193, 83, 155, 14);
+		panel.add(lblNewLabel_1);
 		
 		this.initialize();
 	}
@@ -30,20 +63,9 @@ public class Selector {
 		this.frame.setResizable(true);
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.frame.getContentPane().setBackground(new Color(255, 255, 255));
-		this.frame.setBounds(100, 100, 1000, 310);
-		
-		JButton botonCuatro = crearBoton("4 x 4", 4);
-		JButton botonCinco = crearBoton("5 x 5", 5);
-		JButton botonSiete = crearBoton("7 x 7", 7);
-		
-		panel.add(botonCuatro);
-		panel.add(botonCinco);
-		panel.add(botonSiete);;
+		this.frame.setBounds(300, 100, 550, 350);;
 		
 		this.frame.getContentPane().add(panel);
-		
-		this.botonConfirmarSeleccion = new JButton("Confirmar tamaño");
-		frame.getContentPane().add(botonConfirmarSeleccion, BorderLayout.SOUTH);
 	}
 	
 	public JButton crearBoton(String texto, int tamanio) {
@@ -55,9 +77,8 @@ public class Selector {
 				actualizarSeleccion(boton);
 			}			
 		});
-		boton.setBorderPainted(false);
+		boton.setFont(new Font("Yu Gothic UI", Font.BOLD, 15));
 		boton.setBackground(Color.decode(ColorEstado.APAGADO.getHexa()));
-		boton.setBounds(250, 11, 230, 234);
 		return boton;
 	}
 		
